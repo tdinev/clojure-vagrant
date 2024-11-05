b86b13830f641f04bd82a5c174ef9f6ed8cbd4f0
Todor Dinev <todor.dinev@secu.lu>
2024-11-05 13:52:15 +0100

    feat: Install Leiningen as well

--- custom-root.sh
+++ custom-root.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y curl git emacs rlwrap openjdk-17-jdk-headless tree httpie jq figlet lolcat docker ripgrep
+apt-get install -y curl git emacs rlwrap openjdk-17-jdk-headless tree httpie jq figlet lolcat docker ripgrep leiningen
